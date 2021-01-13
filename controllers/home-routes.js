ef31814253b3ae9278b39f31239a196aefaa8d06
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2021-01-12 22:10:41 -0700

    javascipt for login/signup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 
 router.get('/', (req, res) => {
   Post.findAll({
@@ -35,6 +35,10 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
 module.exports = router;
