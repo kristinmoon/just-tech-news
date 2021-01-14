a43d4efef4772946cac20b7d4133544531fa8ed8
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2021-01-13 22:41:10 -0700

    login and logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -100,7 +100,17 @@ router.post('/login', (req, res) => {
 
       res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
-  });
+  }).catch(err => console.log(err));
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 // PUT /api/users/1
--- server.js
+++ server.js
@@ -3,22 +3,12 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// turn on routes
-app.use(routes);
-
-
-// turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
 
 // set up Handlebars
 const exphbs = require('express-handlebars');
@@ -42,3 +32,11 @@ const sess = {
 };
 
 app.use(session(sess));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
